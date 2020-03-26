54bc18659441a3dba4427eccea239cc7eb316c6a
ivan-piminov <ivan-piminov@yandex.ru>
2020-03-26 19:39:09 +0300

    Updates

---
+++ precache-manifest.692d25ebea6eb902ee0e291ed72a240b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b8114fc7b405c9d19a2e451df7cb31",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b86515e01b9fecc4ffb2",
+    "url": "/portfolio/static/css/main.c77e1633.chunk.css"
+  },
+  {
+    "revision": "af4ce145f0258a22a2e5",
+    "url": "/portfolio/static/js/2.478b19db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.478b19db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86515e01b9fecc4ffb2",
+    "url": "/portfolio/static/js/main.a068b43d.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
