4d6693c782949d9cf0f01e65cc6fd5fb4bb565fa
ivan-piminov <ivan-piminov@yandex.ru>
2020-06-22 21:29:29 +0300

    Updates

---
+++ precache-manifest.83cfe8eb29e38c4ab3c50c6d70252a69.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7061eaeb2fa6d82b64fb7c4dc1bec2f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e15e6c00935de564e18d",
+    "url": "/portfolio/static/css/main.15923c18.chunk.css"
+  },
+  {
+    "revision": "54ddac1018b2edc72ece",
+    "url": "/portfolio/static/js/2.71891dfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.71891dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15e6c00935de564e18d",
+    "url": "/portfolio/static/js/main.51b5e3c8.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "f81a6db38d378c596601e01742c796ea",
+    "url": "/portfolio/static/media/Calc7.f81a6db3.jpg"
+  },
+  {
+    "revision": "117e11f5fc25b16da2921bb951e020cf",
+    "url": "/portfolio/static/media/HTML.117e11f5.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/portfolio/static/media/Instagram2.5b23a837.svg"
+  },
+  {
+    "revision": "6f724616f0c4e4b57445d85ba8f99983",
+    "url": "/portfolio/static/media/JS.6f724616.svg"
+  },
+  {
+    "revision": "7ae9c5eed534a12b90003b68f7b2f2c8",
+    "url": "/portfolio/static/media/React.7ae9c5ee.svg"
+  },
+  {
+    "revision": "d4b26e2e5961b69171b7ef3a03b802ba",
+    "url": "/portfolio/static/media/VK2.d4b26e2e.svg"
+  },
+  {
+    "revision": "db267fdf3f336988650fd73e891f7249",
+    "url": "/portfolio/static/media/WHT2.db267fdf.svg"
+  },
+  {
+    "revision": "acca261eb3c6b9b22a3b57b17fdf58fa",
+    "url": "/portfolio/static/media/scale_1200.acca261e.webp"
+  },
+  {
+    "revision": "b91cc43ec2a72c3e999aff767e2e0c76",
+    "url": "/portfolio/static/media/telegram2.b91cc43e.svg"
+  },
+  {
+    "revision": "a3887df9bc56fb54231da591350ca5af",
+    "url": "/portfolio/static/media/Фон2.a3887df9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.939c2da95692b7c3eb0e61f6f8839fe4.js"
+  "/portfolio/precache-manifest.83cfe8eb29e38c4ab3c50c6d70252a69.js"
 );
 
 self.addEventListener('message', (event) => {
